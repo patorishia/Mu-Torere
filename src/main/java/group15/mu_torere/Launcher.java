fa8365fc5373d9c22d7f1b79b777027bc1208eaa
André Freitas <freitaslandre10@gmail.com>
2026-06-06 20:00:23 +0100

    Adiciona Launcher para corrigir erro do JavaFX e iniciar o jogo

---
+++ src/main/java/group15/mu_torere/Launcher.java
@@ -0,0 +1,8 @@
+package group15.mu_torere;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Isto vai chamar o teu jogo original contornando o bloqueio de módulos
+        Mu_Torere.main(args);
+    }
+}
